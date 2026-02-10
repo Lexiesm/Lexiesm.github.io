adfc196747d232b85c870a4eb7d8e087f3cd09d5
Lexiesm <apsanmartin@uc.cl>
2026-02-10 16:37:42 -0300

    epa

---
+++ index.js
@@ -0,0 +1,10 @@
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+
+yesBtn.addEventListener('click', () => {
+  alert("¡Yay! Sabía que dirías que sí ❤️");
+});
+
+noBtn.addEventListener('click', () => {
+  alert("😢 Oh no… ¡todavía podemos ser amigos!");
+});
